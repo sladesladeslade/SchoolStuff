bda130ff1f817860d644e9c9518be890f47818e1
sladesladeslade <spbrooks4@gmail.com>
2023-01-23 21:26:24 -0500

    spaces?

--- aeroComputing/HW2/pyvot.py
+++ aeroComputing/HW2/pyvot.py
@@ -51,7 +51,7 @@ def gaussPivot(a,b,tol=1.0e-12):
     return b
 
 
-def LUdecomp(a,tol=1.0e-9):
+def LUdecomp(a, tol=1.0e-9):
     """
     a,seq = LUdecomp(a,tol=1.0e-9).
     LU decomposition of matrix [a] using scaled row pivoting.
@@ -68,27 +68,27 @@ def LUdecomp(a,tol=1.0e-9):
     for i in range(n):
         s[i] = max(abs(a[i,:]))
 
-    for k in range(0,n-1):
+    for k in range(0, n - 1):
 
         # Row interchange, if needed
-        p = np.argmax(np.abs(a[k:n,k])/s[k:n]) + k
+        p = np.argmax(np.abs(a[k:n, k])/s[k:n]) + k
         if abs(a[p,k]) < tol: error.err("Matrix is singular")
         if p != k:
-            swap.swapRows(s,k,p)
-            swap.swapRows(a,k,p)
-            swap.swapRows(seq,k,p)
+            swap.swapRows(s, k, p)
+            swap.swapRows(a, k, p)
+            swap.swapRows(seq, k, p)
 
         # Elimination
-        for i in range(k+1,n):
-            if a[i,k] != 0.0:
-                lam = a[i,k]/a[k,k]
-                a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
-                a[i,k] = lam
+        for i in range(k + 1, n):
+            if a[i, k] != 0.0:
+                lam = a[i, k]/a[k, k]
+                a[i, k + 1:n] = a[i, k + 1:n] - lam*a[k, k + 1:n]
+                a[i, k] = lam
 
-    return a,seq
+    return a, seq
 
 
-def LUsolve(a,b,seq):
+def LUsolve(a, b, seq):
     """
     x = LUsolve(a,b,seq).
     Solves [L][U]{x} = {b}, where the matrix [a] = and the
@@ -102,11 +102,11 @@ def LUsolve(a,b,seq):
         x[i] = b[seq[i]]
 
     # Solution
-    for k in range(1,n):
-        x[k] = x[k] - np.dot(a[k,0:k],x[0:k])
-    x[n-1] = x[n-1]/a[n-1,n-1]
-    for k in range(n-2,-1,-1):
-        x[k] = (x[k] - np.dot(a[k,k+1:n],x[k+1:n]))/a[k,k]
+    for k in range(1, n):
+        x[k] = x[k] - np.dot(a[k, 0:k], x[0:k])
+    x[n - 1] = x[n - 1]/a[n - 1, n - 1]
+    for k in range(n - 2, -1, -1):
+        x[k] = (x[k] - np.dot(a[k, k + 1:n], x[k + 1:n]))/a[k,k]
 
     return x
 
