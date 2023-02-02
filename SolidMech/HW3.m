4442424bb1377558b86e013566bd58acbbf713b3
sladesladeslade <spbrooks4@gmail.com>
2023-02-02 10:42:53 -0500

    broke

--- SolidMech/HW3.m
+++ SolidMech/HW3.m
@@ -4,7 +4,7 @@ theta = 0:1:360;
 
 sigman = (22.5)/(.015*.01)*(cos(theta*pi/180)).^2;
 
-taun = [(22.5*cos(theta*pi/180)-22.5*(cos(theta*pi/180)).^3)/(.01*.015) 0 ((-22.5*(cos(theta*pi/180)).^2).*sin(theta*pi/180))/(.01*.015)];
+taun = [];
 
 plot(theta, sigman, linewidth=2)
-%plot(theta, norm(taun), linewidth=2)
+plot(theta, norm(taun), linewidth=2)
