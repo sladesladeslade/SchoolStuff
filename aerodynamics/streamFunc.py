19ab8e191eb358694f06e4b72d0e636c90b2dc53
sladesladeslade <spbrooks4@gmail.com>
2023-02-07 14:52:50 -0500

    work

---
+++ aerodynamics/streamFunc.py
@@ -0,0 +1,38 @@
+# Slade Brooks
+# brooksl@mail.uc.edu
+# 02.07.2023
+
+# stream func class
+
+
+import math
+
+
+class streamFunc():
+    """
+    Collection of functions to calculate
+    """
+    def sourcesinkPsi(self, x1, y1, x2, y2, A):
+        """
+        Calculates the stream function (psi) of a source/sink
+        
+        :param x1: x pos of source
+        :param y1: y pos of source
+        :param x2: x pos of point
+        :param y2: y pos of point
+        :param A: lambda (strength of source/sink)
+        :returns: strength of V @ pt (x2, y2)
+        """
+
+        # calculate angle
+        theta = math.atan((y2-y1)/(x2-x1))
+
+        # calculate strength
+        V = (A/(2*math.pi()))*theta
+
+        return V
+
+
+if __name__ == "__main__":
+    test = streamFunc()
+    #print(test.sourcesinkPSI(1, 1, 2, 2, 0))       # tested angle
