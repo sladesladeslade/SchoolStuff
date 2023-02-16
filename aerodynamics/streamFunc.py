30ae85739369c558a4c219005bec3e3a9f9fd659
sladesladeslade <spbrooks4@gmail.com>
2023-02-16 00:54:43 -0500

    vortex?

--- aerodynamics/streamFunc.py
+++ aerodynamics/streamFunc.py
@@ -44,5 +44,26 @@ def sourcesink(x1, y1, x2, y2, A):
     return V
 
 
+def vortex(x1, y1, x2, y2, gamma):
+    """
+    Calculates the stream function (psi) of a vortex
+    
+    :param x1: x pos of source
+    :param y1: y pos of source
+    :param x2: x pos of point
+    :param y2: y pos of point
+    :param gamma: constant of vortex
+    :returns: strength of V @ pt (x2, y2)
+    """
+
+    # calculate radius
+    r = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    # calculate strength
+    V = (gamma/(2*math.pi))*np.log(r)
+
+    return V
+
+
 if __name__ == "__main__":
     print("test")
