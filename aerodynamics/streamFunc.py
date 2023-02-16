12d464f0b97dd53634ab855cb9a9ccf571352815
sladesladeslade <spbrooks4@gmail.com>
2023-02-16 00:44:27 -0500

    oops

--- aerodynamics/streamFunc.py
+++ aerodynamics/streamFunc.py
@@ -9,32 +9,40 @@ import math
 import numpy as np
 
 
-class streamFunc():
+def uniform(y2, V):
     """
-    Collection of functions to calculate
+    Calculates the stream function (psi) of a uniform flow
+    
+    :param y2: y pos of point
+    :param V: free stream velocity
+    :returns: strength of V @ pt (x2, y2)
     """
 
-    def sourcesinkPsi(self, x1, y1, x2, y2, A):
-        """
-        Calculates the stream function (psi) of a source/sink
-        
-        :param x1: x pos of source
-        :param y1: y pos of source
-        :param x2: x pos of point
-        :param y2: y pos of point
-        :param A: lambda (strength of source/sink)
-        :returns: strength of V @ pt (x2, y2)
-        """
+    V = V*y2
 
-        # calculate angle
-        theta = np.arctan2((y2 - y1),(x2 - x1))
+    return V
 
-        # calculate strength
-        V = (A/(2*(math.pi)))*theta
 
-        return V
+def sourcesink(x1, y1, x2, y2, A):
+    """
+    Calculates the stream function (psi) of a source/sink
+    
+    :param x1: x pos of source
+    :param y1: y pos of source
+    :param x2: x pos of point
+    :param y2: y pos of point
+    :param A: lambda (strength of source/sink)
+    :returns: strength of V @ pt (x2, y2)
+    """
+
+    # calculate angle
+    theta = np.arctan2((y2 - y1),(x2 - x1))
+
+    # calculate strength
+    V = (A/(2*(math.pi)))*theta
+
+    return V
 
 
 if __name__ == "__main__":
-    test = streamFunc()
-    #print(test.sourcesinkPSI(1, 1, 2, 2, 0))       # tested angle
+    print("test")
