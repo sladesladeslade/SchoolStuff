282da2b4ff5e885a41938347f6264f78dfc53374
sladesladeslade <spbrooks4@gmail.com>
2023-02-16 23:16:55 -0500

    pyvot updates

--- aeroComputing/lib/pyvot.py
+++ aeroComputing/lib/pyvot.py
@@ -10,6 +10,7 @@
 import numpy as np
 import sys
 import math
+import matplotlib.pyplot as plt
 
 
 def err(string):
@@ -227,6 +228,167 @@ def conjGrad(Av, x, b, tol=1.0e-9):
             beta = -np.dot(r, u)/np.dot(s, u)
             s = r + beta*s
     return x, i
+
+# newtonPoly
+
+def evalPoly(a, xData, x):
+    """
+    Evaluates Newton's polynomial p at x. The coefficient
+    vector {a} can be computed by the function "coeffts".
+    """
+
+    n = len(xData) - 1
+    p = a[n]
+    for k in range(1, n+1):
+        p = a[n-k] + (x - xData[n-k])*p
+    return p
+
+
+def coeffts(xData, yData):
+    """
+    Computes the coefficients of Newton's polynomial.
+    """
+    m = len(xData)
+    a = yData.copy()
+    for k in range(1, m):
+        a[k:m] = (a[k:m] - a[k-1])/(xData[k:m] - xData[k-1])
+    return a
+
+# rational
+
+def rational(xData, yData, x):
+    """
+    Evaluates the diagonal rational function interpolant p(x)
+    that passes through the data points
+    """
+    m = len(xData)
+    r = yData.copy()
+    rOld = np.zeros(m)
+    for k in range(m-1):
+        for i in range(m-k-1):
+            if abs(x - xData[i+k+1]) < 1.0e-9:
+                return yData[i+k+1]
+            else:
+                c1 = r[i+1] - r[i]
+                c2 = r[i+1] - rOld[i+1]
+                c3 = (x - xData[i])/(x - xData[i+k+1])
+                r[i] = r[i+1] + c1/(c3*(1.0 - c1/c2) - 1.0)
+                rOld[i+1] = r[i+1]
+    return r[0]
+
+# Cubic Spline
+
+def curvatures(xData, yData):
+    """
+    Returns the curvatures of cubic spline at its knots.
+    """
+    n = len(xData) - 1
+    c = np.zeros(n)
+    d = np.ones(n+1)
+    e = np.zeros(n)
+    k = np.zeros(n+1)
+    c[0:n-1] = xData[0:n-1] - xData[1:n]
+    d[1:n] = 2.0*(xData[0:n-1] - xData[2:n+1])
+    e[1:n] = xData[1:n] - xData[2:n+1]
+    k[1:n] = 6.0*(yData[0:n-1] - yData[1:n]) \
+                /(xData[0:n-1] - xData[1:n]) \
+                -6.0*(yData[1:n] - yData[2:n+1]) \
+                /(xData[1:n] - xData[2:n+1])
+    LUdecomp(c, d, e)
+    LUsolve(c, d, e, k)
+    return k
+
+
+def evalSpline(xData, yData, k, x):
+    """
+    Evaluates cubic spline at x. The curvatures k can be
+    computed with the function "curvatures".
+    """
+    def findSegment(xData, x):
+        iLeft = 0
+        iRight = len(xData) - 1
+        while 1:
+            if (iRight - iLeft) <= 1: return iLeft
+            i = (iLeft + iRight)//2
+            if x < xData[i]: iRight = i
+            else: iLeft = i
+
+    i = findSegment(xData, x)
+    h = xData[i] - xData[i+1]
+    y = ((x - xData[i+1])**3/h - (x - xData[i+1])*h)*k[i]/6.0 \
+         - ((x - xData[i])**3/h - (x - xData[i])*h)*k[i+1]/6.0 \
+         + (yData[i]*(x - xData[i+1]) - yData[i+1]*(x - xData[i]))/h
+    return y
+
+# polyFit
+
+def polyFit(xData, yData, m):
+    """
+    Returns coefficients of the polynomial
+    p(x) = c[0] + c[1]x + c[2]xˆ2 +...+ c[m]xˆm
+    that fits the specified data in the least
+    squares sense.
+    """
+    a = np.zeros((m+1, m+1))
+    b = np.zeros(m+1)
+    s = np.zeros(2*m+1)
+    for i in range(len(xData)):
+        temp = yData[i]
+        for j in range(m+1):
+            b[j] = b[j] + temp
+            temp = temp*xData[i]
+        temp = 1.0
+        for j in range(2*m+1):
+            s[j] = s[j] + temp
+            temp = temp*xData[i]
+    for i in range(m+1):
+        for j in range(m+1):
+            a[i, j] = s[i+j]
+    return gaussPivot(a, b)
+
+
+def stdDev(c, xData, yData):
+    """
+    Computes the std. deviation between p(x)
+    and the data.
+    """
+    def evalPoly(c, x):
+        m = len(c) - 1
+        p = c[m]
+        for j in range(m):
+            p = p*x + c[m-j-1]
+        return p
+
+    n = len(xData) - 1
+    m = len(c) - 1
+    sigma = 0.0
+    for i in range(n+1):
+        p = evalPoly(c, xData[i])
+        sigma = sigma + (yData[i] - p)**2
+    sigma = math.sqrt(sigma/(n - m))
+    return sigma
+
+# plotPoly
+
+def plotPoly(xData, yData, coeff, xlab="x", ylab="y"):
+    """
+    Plots data points and the fitting
+    polynomial defined by its coefficient
+    array coeff = [a0, a1. ...]
+    xlab and ylab are optional axis labels
+    """
+    m = len(coeff)
+    x1 = min(xData)
+    x2 = max(xData)
+    dx = (x2 - x1)/20.0
+    x = np.arange(x1, x2 + dx/10.0, dx)
+    y = np.zeros((len(x)))*1.0
+    for i in range(m):
+        y = y + coeff[i]*x**i
+    plt.plot(xData, yData, "o", x, y, "-")
+    plt.xlabel(xlab); plt.ylabel(ylab)
+    plt.grid (True)
+    plt.show()
 
 
 # testing
