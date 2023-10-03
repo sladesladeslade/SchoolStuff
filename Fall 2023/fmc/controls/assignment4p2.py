9994250fb073253710d465fe299cfd4d4a2a17e3
sladesladeslade <spbrooks4@gmail.com>
2023-10-02 20:04:56 -0400

    [cont] Finish A4 i think

--- Fall 2023/fmc/controls/assignment4p2.py
+++ Fall 2023/fmc/controls/assignment4p2.py
@@ -1,72 +1,68 @@
-# AEEM4042 Controls Assignment #1 Part 1
+# AEEM4042 Controls Assignment #1 Part 2
 # Slade Brooks
 # brooksl@mail.uc.edu
 
 import sys
 sys.path.append("C:\\Users\\spbro\\SchoolStuff\\Fall 2023\\fmc\\controls\\lib\\")
 import matplotlib.pyplot as plt
-import lib.mssimparams as P
-import lib.massSpringAnim as animation
-import lib.massSpringDynamics as dynamics
+import lib.VTOLsimparams as P
+import lib.VTOLAnim as animation
+import lib.VTOLDynamics as dynamics
 import keyboard
-import lib.msdPD as ctr
+import lib.vtolPD as ctr
 
 
 # define system
-msd = dynamics.massSpringDynamics()
-anim = animation.massSpringAnim(limits=2, flag=True)
+vtol = dynamics.VTOLDynamics()
+anim = animation.VTOLAnim(limits=10, flag=True)
 ctr = ctr.controller()
 
 # add subplots
 zes = anim.fig.add_subplot(2, 2, 2)
 fes = anim.fig.add_subplot(2, 2, 4)
-zes.set_ylabel("z (m)")
+zes.set_ylabel("h (m)")
 fes.set_ylabel("Force (N)")
 simtimes = []
-zs = []
+hs = []
 fs = []
 targets = []
+target = 0.25
 
-# initials
-z = 0
-u = 0
-target = 0
-
-# simulation loop
+# sim loop
 t = P.t_start
 while t < P.t_end:
     if t <= 2:
-        target = 0
-    elif t <= 20:
-        target = 1
+        target = 0.25
     elif t <= 30:
-        target = 1.5
-    elif t <= 40:
+        target = 5
+    elif t <= 50:
+        target = 8
+    elif t <= 65:
         target = 0.5
     else:
-        target = 0
-        
-    u = ctr.update(target, msd.state)
-    z = msd.update(u)
+        target = 0.25
+    
+    F = ctr.update(target, vtol.state)
+    y = vtol.update(F/2., F/2.)
     
     # update daterp
     simtimes.append(t)
-    zs.append(z)
-    fs.append(u)
+    hs.append(y[1][0])
+    fs.append(F)
     targets.append(target)
     
-    # update animation and plots
-    anim.update(msd.state)
+    # update anim
+    anim.update(vtol.state)
     zes.clear()
     fes.clear()
-    zes.plot(simtimes, zs, label="State")
+    zes.plot(simtimes, hs, label="State")
     fes.plot(simtimes, fs)
     zes.plot(simtimes, targets, label="Target")
     zes.legend(loc="lower right")
     zes.grid()
     fes.grid()
     zes.set_ylabel("z (m)")
-    fes.set_ylabel("Force (N)")
+    fes.set_ylabel("Total Force (N)")
     plt.pause(0.01)
     
     t += P.Ts
