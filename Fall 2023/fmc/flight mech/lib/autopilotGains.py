036c9408e4c7a748aa9704d5dad4bf0fef99a4da
sladesladeslade <spbrooks4@gmail.com>
2023-11-08 21:32:49 -0500

    [cont] rise times broken

--- Fall 2023/fmc/flight mech/lib/autopilotGains.py
+++ Fall 2023/fmc/flight mech/lib/autopilotGains.py
@@ -34,22 +34,22 @@ a_V3 = P.gravity * np.cos(theta_trim - alpha_trim)
 zeta = .707
 
 # roll
-tr_roll = 1
+tr_roll = 10.
 wn_roll = 2.2/tr_roll
 kp_roll = (wn_roll**2)/a_phi1
 kd_roll = (2*zeta*wn_roll - a_phi1)/a_phi2
 ki_roll = 0
 
 # course hold
-tr_course = 1
+tr_course = 30.
 wn_course = 2.2/tr_course
 kp_course = (2*zeta*wn_course*Va_trim)/P.gravity
 kd_course = 0
 ki_course = ((wn_course**2)*Va_trim)/P.gravity
 
 # pitch attitude hold
-zeta_pitch = .1
-tr_pitch = .1
+zeta_pitch = 0.1
+tr_pitch = 0.1
 wn_pitch = 2.2/tr_pitch
 kp_pitch = ((wn_pitch**2) - a_theta2)/a_theta3
 kd_pitch = (2*zeta_pitch*wn_pitch - a_theta1)/a_theta3
@@ -57,21 +57,21 @@ ki_pitch = 0
 ktheta_DC = (kp_pitch*a_theta3)/(a_theta2 + kp_pitch*a_theta3)
 
 # altitude from pitch gain
-tr_altitude = 1
+tr_altitude = 1.
 wn_altitude = 2.2/tr_altitude
 kp_altitude = (2*zeta*wn_altitude)/(ktheta_DC*Va_trim)
 kd_altitude = 0
 ki_altitude = (wn_altitude**2)/(ktheta_DC*Va_trim)
 
 # airspeed from pitch
-tr_airspeed = 1
+tr_airspeed = 1.
 wn_airspeed = 2.2/tr_airspeed
 kp_airspeed = (a_V1 - 2*zeta*wn_airspeed)/ktheta_DC
 kd_airspeed = 0
 ki_airspeed = (wn_airspeed**2)/(ktheta_DC*P.gravity)
 
 # airspeed from throttle
-tr_throttle = .1
+tr_throttle = 0.1
 wn_throttle = 2.2/tr_throttle
 kp_throttle = (2*zeta*wn_throttle - a_V1)/a_V2
 kd_throttle = 0
