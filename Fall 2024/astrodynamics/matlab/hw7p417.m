d0ae572e87b425ae7ff3a9cbbc2aa9733c52825c
sladesladeslade <spbrooks4@gmail.com>
2024-10-30 14:24:28 -0400

    [astro] hw7

---
+++ Fall 2024/astrodynamics/matlab/hw7p417.m
@@ -0,0 +1,16 @@
+%% Initialize
+clear; clc;
+
+%% Define variables and constants
+global mu
+mu = 398600;        % km^3/s^2
+
+%% Initial conditions
+R0 = [-1984 -5348 3471];       % km
+V0 = [10.36 -5.763 -2.961];       % km/s
+t = 50*60;                          % s
+
+%% do algortihm (from appendix D.16) and output results
+[R, V] = rv_from_r0v0(R0, V0, t);
+fprintf("Final position: R=(%g, %g, %g) km", R(1), R(2), R(3));
+fprintf("\nFinal velocity: V=(%g, %g, %g) km/s", V(1), V(2), V(3));
