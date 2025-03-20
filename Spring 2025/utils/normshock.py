8b271580e0182cd386ed8b3580473c8f01ee81fe
Slade <spbrooks4@gmail.com>
2025-03-20 19:09:51 -0400

    [atavp] trying to fix stuff

---
+++ Spring 2025/utils/normshock.py
@@ -0,0 +1,47 @@
+# Normal Shock Functions
+# Slade Brooks
+# brooksl@mail.uc.edu
+
+import numpy as np
+
+
+def normshock(Mx:float, y:float):
+    """
+    Function to calculate change in flow parameters across normal shock.
+    
+    Parameters
+    ----------
+    Mx : float
+        Upstream Mach number.
+    y : float
+        Specific heat ratio.
+        
+    Returns
+    -------
+    My : float
+        Downstream Mach number.
+    Pty_Ptx : float
+        Total pressure ratio (= sonic area ratio).
+    Py_Px : float
+        Static pressure ratio.
+    rhoy_rhox : float
+        Density ratio (= velocity ratio).
+    Ty_Tx : float
+        Static temperature ratio.
+    """
+    # calculate My
+    My = np.sqrt((Mx**2 + 2/(y - 1))/(2*y/(y - 1)*Mx**2 - 1))
+    
+    # calculate total pressure ratio
+    Pty_Ptx = ((((y + 1)/2*Mx**2)/(1 + (y - 1)/2*Mx**2))**(y/(y - 1)))*((2*y/(y + 1)*Mx**2 - (y - 1)/(y + 1))**(-1/(y - 1)))
+    
+    # calculate static pressure ratio
+    Py_Px = 2*y/(y + 1)*Mx**2 - (y - 1)/(y + 1)
+    
+    # calculate static temperature ratio
+    Ty_Tx = (2*y*Mx**2 - (y - 1))*((y - 1)*Mx**2 + 2)/(((y + 1)**2)*(Mx**2))
+    
+    # calculate density ratio
+    rhoy_rhox = Py_Px/Ty_Tx
+    
+    return np.round(My, 6), np.round(Pty_Ptx, 6), np.round(Py_Px, 6), np.round(rhoy_rhox, 6), np.round(Ty_Tx, 6)
